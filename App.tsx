004a3c9fe222ea3455e967ad89e0b8c00ff15683
Achint Srivastava <32853258+BabaYaga007@users.noreply.github.com>
2024-06-15 16:10:48 +0530

    A big Update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './components/AppNavigator';
+// import CreateTable from './components/GynoApp/DBfiles/CreateTable';
+import { AppProvider } from './components/AppContext';
 
 function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    //For Adding context
+    <AppProvider> 
+      <NavigationContainer>
+        {/* <CreateTable /> */}
+        {/*For creating tables in DB  */}
+        <AppNavigator />
+      </NavigationContainer>
+    </AppProvider>
   );
-};
+}
 
 export default App;
